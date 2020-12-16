fd50e251cc21c56dd13b442e6bc1f224c0503653
abc <abc@example.com>
2020-12-16 23:57:44 +0530

    Ui updated

--- src/Feed.js
+++ src/Feed.js
@@ -2,6 +2,11 @@ import React ,{useState} from "react";
 import "./Feed.css"
 import Post from "./Post";
 
+import CropOriginalIcon from '@material-ui/icons/CropOriginal';
+import GifIcon from '@material-ui/icons/Gif';
+import PollIcon from '@material-ui/icons/Poll';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
 
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
@@ -44,13 +49,22 @@ function setNewTweet(event){
             </div>
             <form className="tweet-box-block">
             <div className="tweet-box">
-            <Avatar className="profile-avatar" 
+            <Avatar 
             src="https://pbs.twimg.com/profile_images/1065633264084402177/DD-DVJp4_400x400.jpg"/>
             <input className="tweet-input" 
             placeholder="What's happening?" type="url" name="text" value={tweet.text} onChange={handleChange} />
             </div>
             <input placeholder="Enter Image Url" type="url" name="image" value={tweet.image} className="image-url" onChange={handleChange} />
+            <div className="bottom-section">
+            <div className="tweet-box-icons">
+                <CropOriginalIcon className="tweet-box-icon" />
+                <GifIcon className="tweet-box-icon" />
+                <PollIcon className="tweet-box-icon" />
+                <InsertEmoticonIcon className="tweet-box-icon" />
+                <CalendarTodayIcon className="tweet-box-icon" />
+            </div>
             <Button type="submit" onClick={setNewTweet}  className="tweet-box-btn">Tweet</Button>
+            </div>
             </form>
             {tweets.map((item,index)=>(
                  <Post key={index} newTweetText={item.text} newTweetImage={item.image} />))}
--- src/Post.js
+++ src/Post.js
@@ -2,6 +2,12 @@ import "./Post.css";
 import React  from "react";
 import Avatar from "@material-ui/core/Avatar";
 import VerifiedUserIcon from '@material-ui/icons/VerifiedUser'; 
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import PublishIcon from '@material-ui/icons/Publish';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+
 function  Post({newTweetText,newTweetImage}) {
 
     return <div className="post">
@@ -10,12 +16,19 @@ function  Post({newTweetText,newTweetImage}) {
         <h2 className="display-name">Omkar</h2>
       <VerifiedUserIcon className="verified-icon"/>
         <h3>@omkargodham</h3>
+        <MoreHorizIcon className="post-more-icon"/>
       </div>
       <div className="actual-tweet">
           <p>{newTweetText}</p>
           <img src={newTweetImage} className="tweet-image"/>
       </div>
-    </div>
+      <div className="reaction-icons">
+        <ChatBubbleOutlineIcon className="reaction-icon"/>
+        <RepeatIcon  className="reaction-icon"/>
+        <FavoriteBorderIcon   className="reaction-icon"/>
+        <PublishIcon className="reaction-icon"/> 
+      </div>
+      </div>
 
 }
 export default Post;
